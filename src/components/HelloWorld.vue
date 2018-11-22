49298c10aee41a4f425bdb3419fdeac6ac6415bd
Raghav Toshniwal <raghav.toshniwal@gmail.com>
2018-11-22 18:18:36 +0530

    First working version

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,59 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="messagebox">
+      <textarea v-model="tweet" v-on:input="tweetChange()"></textarea>
+    </div>
+    <select v-model="algo" v-on:input="tweetChange()">
+      <option v-for="algo in algos" v-bind:value="algo">
+        {{ algo }}
+      </option>
+    </select>
+    <div>
+      <div>Message: {{tweet}}</div>
+      <div>Hash: {{hash}}</div>
+      <div>Algorithm: {{algo}}</div>
+    </div>
+    <a :href="composedMessage">Tweet it!</a>
   </div>
 </template>
 
 <script>
+import crypto from 'crypto';
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      msg: "Welcome to Tweet Encryptor",
+      tweet:'',
+      preMessage:'https://twitter.com/intent/tweet?text=',
+      composedMessage:'',
+      hash:'',
+      algos:['MD5','RIPEMD160','SHA1','SHA224','SHA256','SHA384','SHA512'],
+      algo:'SHA256'
+    }
+  },
+  methods: {
+    tweetChange(){
+      if(this.tweet!==''){
+        this.computeHash()
+        this.composedMessage=this.preMessage+this.hash
+      } 
+      else this.hash
+    },
+    computeHash(){
+      const secret = '';
+      this.hash = crypto.createHash(this.algo, secret)
+                 .update(this.tweet)
+                 .digest('hex');
+    }
+  },
+  watch: {
+    algo: function (newAlgo){
+      this.algo=newAlgo
+      this.computeHash()
+    }
   }
+
 }
 </script>
 
